d301cb88b9d3513b896d75c3bf4258f745571bf0
Stephen Stack <sstack@punchthrough.com>
2016-09-13 00:10:27 -0500

    log_serial: sketch logs different types of serial data

---
+++ sketches/log_serial/log_serial.ino
@@ -0,0 +1,44 @@
+#define START_FRAME 0x77
+#define COMMAND_SERIAL_BINARY 0
+#define COMMAND_SERIAL_ASCII 1
+#define COMMAND_SERIAL_LONG 2
+
+
+// Data definitions
+uint8_t binaryData[3] = {1, 2, 3};
+char *shortStr = "foo";
+char *longStr = "111222333444555666777888999";
+
+void setup() {
+  Serial.begin();
+}
+
+
+void sendCommand(int command) {
+  switch (command) {
+    case COMMAND_SERIAL_BINARY:
+      Serial.write(binaryData, 3);
+      break;
+    case COMMAND_SERIAL_ASCII:
+      Serial.write(shortStr);
+      break;
+    case COMMAND_SERIAL_LONG:
+      Serial.write(longStr);
+      break;
+    default:
+      break;
+
+  }
+}
+
+
+void loop() {
+
+  sendCommand(COMMAND_SERIAL_BINARY);
+  Bean.sleep(1000);
+  sendCommand(COMMAND_SERIAL_ASCII);
+  Bean.sleep(1000);
+  sendCommand(COMMAND_SERIAL_LONG);
+
+  Bean.sleep(5000);
+}
